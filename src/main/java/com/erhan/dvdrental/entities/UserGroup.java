ccbcbbb79dfa7aac2d068e92870f12b25da74759
Erhan TUNCEL <erhan.glstrme@gmail.com>
2018-12-10 12:50:37 +0300

    Added: UserGroup entity class and UserGroupFacade ejb class.

---
+++ src/main/java/com/erhan/dvdrental/entities/UserGroup.java
@@ -0,0 +1,80 @@
+package com.erhan.dvdrental.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_groups")
+public class UserGroup implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @Column(name = "username", nullable = false, length = 16)
+    private String username;
+    
+    @Column(name = "groupname", nullable = false, length = 255)
+    private String groupname;
+
+    public UserGroup() {
+    }
+    
+    public UserGroup(String username, String groupname) {
+        this.username = username;
+        this.groupname = groupname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getGroupname() {
+        return groupname;
+    }
+
+    public void setGroupname(String groupname) {
+        this.groupname = groupname;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.username);
+        hash = 67 * hash + Objects.hashCode(this.groupname);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final UserGroup other = (UserGroup) obj;
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        if (!Objects.equals(this.groupname, other.groupname)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "UserGroups{" + "username=" + username + ", groupname=" + groupname + '}';
+    }
+}
---
+++ src/main/java/com/erhan/dvdrental/jpa/facade/UserGroupFacade.java
@@ -0,0 +1,29 @@
+package com.erhan.dvdrental.jpa.facade;
+
+import com.erhan.dvdrental.entities.UserGroup;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class UserGroupFacade extends AbstractFacade<UserGroup> {
+    
+    @PersistenceContext(unitName = "dvdrental")
+    private EntityManager em;
+
+    public UserGroupFacade() {
+        super(UserGroup.class);
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    @Override
+    public void create(UserGroup entity) {
+        
+    }
+    
+    
+}
