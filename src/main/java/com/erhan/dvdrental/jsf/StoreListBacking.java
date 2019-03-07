8d41ef2019fbc4e925aeca395daae99799f47301
Erhan TUNCEL <erhan.glstrme@gmail.com>
2019-03-08 01:25:17 +0300

    Added: store list page.

---
+++ src/main/java/com/erhan/dvdrental/jsf/StoreListBacking.java
@@ -0,0 +1,49 @@
+package com.erhan.dvdrental.jsf;
+
+import com.erhan.dvdrental.entities.Store;
+import com.erhan.dvdrental.jpa.facade.StoreFacade;
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named("storeListBacking")
+@ViewScoped
+public class StoreListBacking implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @EJB
+    private StoreFacade storeFacade;
+    private List<Store> storeList = null;
+    
+    private List<Store> filteredStores;
+
+    public StoreFacade getStoreFacade() {
+        return storeFacade;
+    }
+
+    public void setStoreFacade(StoreFacade storeFacade) {
+        this.storeFacade = storeFacade;
+    }
+
+    public List<Store> getStoreList() {
+        if(storeList == null) {
+            storeList = getStoreFacade().findAll();
+        }
+        return storeList;
+    }
+
+    public void setStoreList(List<Store> storeList) {
+        this.storeList = storeList;
+    }
+
+    public List<Store> getFilteredStores() {
+        return filteredStores;
+    }
+
+    public void setFilteredStores(List<Store> filteredStores) {
+        this.filteredStores = filteredStores;
+    }
+}
