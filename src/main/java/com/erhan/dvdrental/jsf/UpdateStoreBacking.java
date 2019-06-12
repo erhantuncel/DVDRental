22ddf7ea3f84b7e9e3149d370de6e0725452ea71
Erhan TUNCEL <erhan.glstrme@gmail.com>
2019-06-12 17:03:17 +0300

    Added: notification when store is updated.

--- src/main/java/com/erhan/dvdrental/jsf/UpdateStoreBacking.java
+++ src/main/java/com/erhan/dvdrental/jsf/UpdateStoreBacking.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.ValueChangeEvent;
@@ -119,9 +120,16 @@ public class UpdateStoreBacking implements Serializable {
         }
     }
     
-    public void updateStore() {
+    public String updateStore() {
         selectedStore.setLastUpdate(new Date());
         getStoreFacade().edit(selectedStore);
+        
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getExternalContext().getFlash().setKeepMessages(true);
+        context.addMessage("actionResult", new FacesMessage(FacesMessage.SEVERITY_INFO, 
+                selectedStore.getStoreId() + " numaralı şube güncellendi.", 
+                null));
+        return "stores?faces-redirect=true";
     }
     
     public void updateManagerSelectedStore() {
